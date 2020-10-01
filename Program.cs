3ccda55589b3441d00bc2cbc330e3b6140f3b10e
Prashan Patel <prashanpatel2@hotmail.com>
2020-10-01 21:16:41 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Device.Gpio;
+
+namespace SimpleButton
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            GpioController controller = new GpioController(PinNumberingScheme.Logical);
+            var pin = 27;
+            var buttonPin = 17;
+            
+            controller.OpenPin(pin, PinMode.Output);
+            controller.OpenPin(buttonPin, PinMode.InputPullUp);            
+
+            try
+            {
+                while (true)
+                {
+                    if (controller.Read(buttonPin) == false)
+                    {
+                        controller.Write(pin, PinValue.High);
+                    }
+                    else
+                    {
+                        controller.Write(pin, PinValue.Low);
+                    }
+                }
+            }
+            finally
+            {
+                controller.ClosePin(pin);
+            }
+        }
+    }
+}
